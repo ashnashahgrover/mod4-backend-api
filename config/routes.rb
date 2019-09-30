68c0fd4c93e23b9c3e82840ea34d97fe97815f5f
Alessandra Vertrees <avertrees447@gmail.com>
2019-09-30 10:56:27 -0400

    fixed typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :user_top100_tracks, only: [:index, :create]
   resources :top100_tracks, only: [:index, :create]
-  resources :users, only: [:index, :create, :show]
+  resources :users, only: [:index, :create, :update, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
